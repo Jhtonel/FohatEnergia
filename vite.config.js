ec7a129bb0622a8556dbab2789219b0a369cbb09
José Henrique <Jhtonel@gmail.com>
2025-05-24 17:57:26 -0300

    build: update with correct base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -184,7 +184,7 @@ logger.error = (msg, options) => {
 export default defineConfig({
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
-	base: '',
+	base: '/FohatEnergia/',
 	server: {
 		cors: true,
 		headers: {
